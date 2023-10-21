ea29c3d1fc3dead4e3af1ee92914a81c8e0c3a0a
Alice Hong <alicehon@andrew.cmu.edu>
2023-10-21 18:25:46 -0400

    Initial Commit

---
+++ Technica/TechnicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechnicaApp.swift
+//  Technica
+//
+//  Created by Alice Hong on 10/21/23.
+//
+
+import SwiftUI
+
+@main
+struct TechnicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
