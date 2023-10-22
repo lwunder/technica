a952a4686dab6f3c00b8e992aa985917333afc96
Alice Hong <alicehon@andrew.cmu.edu>
2023-10-21 22:03:17 -0400

    Create basic view for Home feed

--- Technica/Views/HomeView.swift
+++ Technica/Views/HomeView.swift
@@ -8,13 +8,25 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State var favors = [String]()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ForEach(favors, id: \.self) { favor in
+                ZStack {
+                    Text("FriendX \(favor)!")
+                        .padding(25)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10.0)
+                                .stroke(Color.gray)
+                        )
+                }
+            }
+        }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        HomeView(favors: ["walked your dog", "watered your plant"])
     }
 }
